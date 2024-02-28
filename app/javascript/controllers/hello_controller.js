df0df83d46f59e01eb80718b7a3aa677d32ecf5c
smic29 <sibulo1991@gmail.com>
2024-02-28 16:47:00 +0800

    Changed login/sign up and used stimulus to update classList

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  
   connect() {
-    this.element.textContent = "Hello World!"
+    this.updateActiveLinks()
+  }
+
+  turboLoad() {
+    this.updateActiveLinks()
+  }
+  
+  updateActiveLinks() {
+    const links = this.element.querySelectorAll(".nav-link")
+    const frame = this.element.querySelector("#login_signup_frame")
+    
+    links.forEach(link => {
+      
+      link.addEventListener('click',() => {
+         links.forEach(otherLink => {
+          otherLink.classList.remove("active")
+         })
+
+         link.classList.add("active")
+        
+      })
+    })
   }
 }
