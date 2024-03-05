2f1a597643376f9c5c27f0fcb7b6f862b80cef45
smic29 <sibulo1991@gmail.com>
2024-03-05 21:18:46 +0800

    Cleaned up accordion_controller.js

--- app/javascript/controllers/accordion_controller.js
+++ app/javascript/controllers/accordion_controller.js
@@ -5,27 +5,17 @@ export default class extends Controller {
   connect() {
     document.addEventListener('turbo:before-stream-render', (e) => {
       const turboStream = e.target;
-
       const template = turboStream.querySelector('template');
-      
       const content = template.content
-      console.log(content)
       const updateId = content.querySelector('turbo-frame').id
 
       if (this.element.id === updateId){
         const button = content.querySelector(`#button_${updateId}`)
         button.classList.remove('collapsed')
 
-        console.log(button)
-
         const div = content.querySelector(`#collapse${updateId}`)
         div.classList.add('show')
       }
-      // const button = content.querySelector('.accordion-button');
-      // button.classList.remove('collapsed');
-
-      // const lastDiv = content.querySelector('.accordion-collapse collapse');
-      // lastDiv.classList.add('show');
     })
   }
 }
