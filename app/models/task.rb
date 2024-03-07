207c9d5e2dcf019e781b8d6508838f520b535edc
smic29 <sibulo1991@gmail.com>
2024-03-07 20:50:58 +0800

    Added finish_by column in new task form, and added validation in model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,13 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  validates :title, :finish_by , presence: true
+  validates :title, presence: true
+  validate :finish_by_in_future
+
+  def finish_by_in_future
+    return if finish_by.blank? || finish_by >= Date.today
+    errors.add(:finish_by, "must be today or in the future")
+  end
 
   # broadcasts_to ->(task) { :task_list }
   # Maybe look into this in the future.
