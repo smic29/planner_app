fc5bfdd1e15463f7b216aade67b6454d7a2499d5
smic29 <sibulo1991@gmail.com>
2024-03-07 21:38:27 +0800

    Added dependent destroy to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,12 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
 
   broadcasts_to ->(category) { :cat_list }
   after_create_commit -> {
-    broadcast_prepend_to :cat_list,
+    broadcast_append_to :cat_list,
     partial: "categories/category",
     locals: { category: self}, target: "cat_list" }
 end
