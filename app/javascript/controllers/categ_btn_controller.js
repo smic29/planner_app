52a739d593f3561c852d8b98ff70779b4efc06ea
smic29 <sibulo1991@gmail.com>
2024-03-09 00:29:40 +0800

    Changed styles for category index

--- app/javascript/controllers/categ_btn_controller.js
+++ app/javascript/controllers/categ_btn_controller.js
@@ -9,8 +9,7 @@ export default class extends Controller {
 
     home_btn.addEventListener('click',() => {
       this.buttonTargets.forEach((btn) => {
-        btn.classList.remove('btn-primary', 'text-white', 'pe-none')
-        btn.classList.add('btn-outline-primary', 'link-dark')
+        this.add_remove_class(btn)
       })
     })
   }
@@ -18,13 +17,23 @@ export default class extends Controller {
   add_active(e) {
     
     this.buttonTargets.forEach((btn) => {
-      btn.classList.remove('btn-primary', 'text-white', 'pe-none')
-      btn.classList.add('btn-outline-primary', 'link-dark')
+      this.add_remove_class(btn)
     })
     
     let active_button = e.target
     active_button.classList.remove('btn-outline-primary', 'link-dark')
     active_button.classList.add('btn-primary', 'text-white', 'pe-none')
-    
+
+    let del_btn = active_button.nextElementSibling
+    let btn = del_btn.querySelector('button')
+
+    btn.classList.remove('visually-hidden')
+  }
+
+  add_remove_class(btn) {
+    btn.classList.remove('btn-primary', 'text-white', 'pe-none')
+    btn.classList.add('btn-outline-primary', 'link-dark')
+
+    btn.nextElementSibling.querySelector('button').classList.add('visually-hidden')
   }
 }
