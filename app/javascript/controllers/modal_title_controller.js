bcc457ce8a6b685f24fec6d007a7104b68f02da4
smic29 <sibulo1991@gmail.com>
2024-03-08 20:45:09 +0800

    Added a stimulus controller that dynamically updates modal title

---
+++ app/javascript/controllers/modal_title_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="modal-title"
+export default class extends Controller {
+  connect() {
+    const turboFrame = this.element.querySelector('turbo-frame')
+    const title = this.element.querySelector('#signedInModalLabel')
+    
+    turboFrame.addEventListener('turbo:frame-load', (e) => {
+      const url = e.target.src 
+      
+      console.log(url.split('/'))
+      
+      if (this.urlMatchesPattern(url, ['users','new'])) {
+        title.innerHTML = "Add a new Category"
+      }  
+      
+      if (this.urlMatchesPattern(url, ['edit','tasks'])) {
+        title.innerHTML = "Edit a Task"
+      }
+
+      if (this.urlMatchesPattern(url, ['new','tasks'])) {
+        title.innerHTML = "Add a new Task"
+      }
+    })
+  }
+
+  urlMatchesPattern(url, patterns) {
+    const segments = url.split('/')
+    return patterns.every(pattern => segments.includes(pattern))
+  }
+
+  
+}
