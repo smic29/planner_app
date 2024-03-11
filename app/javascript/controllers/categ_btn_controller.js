0e627a8635aea83a2c7ace3e52838f190c62d135
smic29 <sibulo1991@gmail.com>
2024-03-11 19:13:38 +0800

    Fixed bug where broadcast on category updates will remove active classes

--- app/javascript/controllers/categ_btn_controller.js
+++ app/javascript/controllers/categ_btn_controller.js
@@ -2,10 +2,43 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="categ-btn"
 export default class extends Controller {
-  static targets = [ "button" ]
+  static targets = [ "button", "div" ]
 
   connect() {
     const home_btn = document.querySelector('#navHome')
+    
+    document.addEventListener('turbo:before-stream-render', (e) => {
+      const task_frame = document.querySelector('#tasks_list')
+      const current_frame_url = task_frame.src.split('/').slice(-2).toString()
+      const is_on_dash = task_frame.src.split('/').slice(-1).toString() == 'dashboard'
+
+      const turboStream = e.target;
+      const template = turboStream.querySelector('template')
+      const content = template.content
+
+      // console.log(turboStream.querySelector('template'))
+      this.divTargets.forEach((div) => {
+        if (turboStream.target == div.id) {
+          const active_a = content.querySelector('a')
+          const active_url = active_a.href.split('/').slice(-2).toString()
+          const is_on_same_frame = active_url === current_frame_url
+          const is_on_diff_frame = active_url !== current_frame_url
+          console.log(`Frame: ${current_frame_url} || Button: ${active_url}`)
+          console.log(active_url === current_frame_url)
+
+          if(!is_on_dash && is_on_same_frame ) {
+            active_a.classList.remove('btn-outline-primary', 'link-dark')
+            active_a.classList.add('btn-primary', 'text-white', 'pe-none')
+    
+            const active_b = active_a.nextElementSibling
+            const button_itself = active_b.querySelector('button')
+    
+            button_itself.classList.remove('visually-hidden')
+          }
+        }
+      })
+    })
+  
 
     home_btn.addEventListener('click',() => {
       this.buttonTargets.forEach((btn) => {
@@ -15,7 +48,7 @@ export default class extends Controller {
   }
 
   add_active(e) {
-    
+
     this.buttonTargets.forEach((btn) => {
       this.add_remove_class(btn)
     })
